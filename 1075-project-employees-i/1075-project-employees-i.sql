bec004f064314cddfcdf7a20b8532436f4dce784
AADHI PRANESH SS <aadhipraneshss.23cse@kongu.edu>
2026-06-28 11:42:09 +0530

    Time: 540 ms (49.26%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select project_id , round(ifnull(sum(e.experience_years)/count(p.project_id),0),2) as  average_years
+from Project p 
+left join Employee e
+on p.employee_id=e.employee_id
+group by project_id ;
